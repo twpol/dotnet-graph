a2c41a8adf33c3da31574fef2d6a6af3b23eb475
James Ross <git@james-ross.co.uk>
2021-11-01 17:37:09 +0000

    feat: Generate non-directed graph of type links from entry point

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace DotNet_Graph
@@ -11,12 +14,44 @@ namespace DotNet_Graph
         {
             MSBuildLocator.RegisterDefaults();
             var workspace = MSBuildWorkspace.Create();
-            var solution = await workspace.OpenSolutionAsync(path);
-            Console.WriteLine($"Loaded solution {path}");
-            foreach (var project in workspace.CurrentSolution.Projects)
+            await workspace.OpenSolutionAsync(path);
+
+            var compilation = await workspace.CurrentSolution.Projects.First().GetCompilationAsync();
+            var global = compilation.GlobalNamespace;
+
+            var rootClass = compilation.GetEntryPoint(System.Threading.CancellationToken.None).ContainingType;
+            var visited = new HashSet<string>();
+            Console.WriteLine("graph {");
+            DumpType(rootClass, visited);
+            Console.WriteLine("}");
+        }
+
+        static void DumpType(INamespaceOrTypeSymbol symbol, ISet<string> visited)
+        {
+            var name = symbol.ToDisplayString();
+            if (visited.Contains(name)) return;
+            visited.Add(name);
+
+            var referencedTypes = new HashSet<string>();
+            var newSymbols = new HashSet<INamespaceOrTypeSymbol>(SymbolEqualityComparer.Default);
+
+            foreach (var member in symbol.GetMembers())
             {
-                Console.WriteLine($"Project {project.Name}");
+                if (member is IFieldSymbol field)
+                {
+                    var type = field.Type;
+                    if (type.Locations.Any(location => location.IsInSource))
+                    {
+                        var innerName = type.ToDisplayString();
+                        if (!referencedTypes.Contains(innerName) && innerName != name) referencedTypes.Add(innerName);
+                        newSymbols.Add(type);
+                    }
+                }
             }
+
+            foreach (var referencedType in referencedTypes) Console.WriteLine($@"  ""{name}"" -- ""{referencedType}""");
+
+            foreach (var newSymbol in newSymbols) DumpType(newSymbol, visited);
         }
     }
 }
